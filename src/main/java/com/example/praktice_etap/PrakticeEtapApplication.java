2b2e6ab89b573b7dab09daa018cebd8e65f5e962
KurbanbaevArlen <kurbanbaevarlen001>
2022-07-21 09:10:54 +0300

    Initial commit

---
+++ src/main/java/com/example/praktice_etap/PrakticeEtapApplication.java
@@ -0,0 +1,14 @@
+package com.example.praktice_etap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrakticeEtapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrakticeEtapApplication.class, args);
+    }
+    
+
+}
---
+++ src/test/java/com/example/praktice_etap/PrakticeEtapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praktice_etap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrakticeEtapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
